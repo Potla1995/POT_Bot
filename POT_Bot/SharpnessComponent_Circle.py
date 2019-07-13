246d87cf21f8db1dd12d74292980c4a162239bbf
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-07-13 09:53:40 +0530

    Formatting Changes to Circle Components.

--- POT_Bot/SharpnessComponent_Circle.py
+++ POT_Bot/SharpnessComponent_Circle.py
@@ -2,65 +2,68 @@ import math
 from statistics import mean
 import os
 
-def BeatInterval(mapID): #Identifies the BPM of any given map.
-  BPM = 0
-  with open(mapID,'r',encoding='utf-8') as f:
-    L = f.read().split('\n')
-    TimingPointIndex = L.index('[TimingPoints]') + 1
-    BPM = float(L[TimingPointIndex].split(',')[1])
-  #60*1000/float(BPM) is the formula for obtaining the true BPM. 
-  return 60*1000/BPM
+
+def BeatInterval(mapID):  # Identifies the BPM of any given map.
+    BPM = 0
+    with open(mapID, "r", encoding="utf-8") as f:
+        L = f.read().split("\n")
+        TimingPointIndex = L.index("[TimingPoints]") + 1
+        BPM = float(L[TimingPointIndex].split(",")[1])
+    # 60*1000/float(BPM) is the formula for obtaining the true BPM.
+    return 60 * 1000 / BPM
+
 
 def DectectStream(mapID):
-  with open(mapID,'r',encoding='utf-8') as f:
-    L = f.read().split("\n")
-    ObjectIndex = L.index('[HitObjects]') + 1
-    
-    
-    CircleIndex = list(x for x in L[ObjectIndex::] if len(x.split(',')) < 7)
-    
-    BPM = BeatInterval(mapID)
-    
-    StreamList = []
-    
-    i = 0
-  
-    while i < len(CircleIndex) -2:
-      Circle = CircleIndex[i].split(',')
-      CircleTime = Circle[2]
-      
-      Circle2 = CircleIndex[i+1].split(',')
-      CircleTime2 = Circle2[2]
-      
-      if(float(CircleTime2) - float(CircleTime) <= BPM):
-        #That means they are together.
-        CurrentStream = []
-        CurrentStream.append(CircleIndex[i])
-        while(float(CircleTime2) - float(CircleTime) < BPM):
-          CurrentStream.append(CircleIndex[i+1])
-          i = i+1
-          if(i >= len(CircleIndex) - 2):
-            break
-          Circle = CircleIndex[i].split(',')
-          CircleTime = Circle[2]
+    with open(mapID, "r", encoding="utf-8") as f:
+        L = f.read().split("\n")
+        ObjectIndex = L.index("[HitObjects]") + 1
 
-          Circle2 = CircleIndex[i+1].split(',')
-          CircleTime2 = Circle2[2]
-      
-        #Steam Obtained, Push to SteamList
-        StreamList.append(CurrentStream)
-      i = i+1
-    
-    TrueStreamList = list(x for x in StreamList if len(x)>3)
+        CircleIndex = list(x for x in L[ObjectIndex::] if len(x.split(",")) < 7)
 
-    return TrueStreamList;      
+        BPM = BeatInterval(mapID)
+
+        StreamList = []
+
+        i = 0
+
+        while i < len(CircleIndex) - 2:
+            Circle = CircleIndex[i].split(",")
+            CircleTime = Circle[2]
+
+            Circle2 = CircleIndex[i + 1].split(",")
+            CircleTime2 = Circle2[2]
+
+            if float(CircleTime2) - float(CircleTime) <= BPM:
+                # That means they are together.
+                CurrentStream = []
+                CurrentStream.append(CircleIndex[i])
+                while float(CircleTime2) - float(CircleTime) < BPM:
+                    CurrentStream.append(CircleIndex[i + 1])
+                    i = i + 1
+                    if i >= len(CircleIndex) - 2:
+                        break
+                    Circle = CircleIndex[i].split(",")
+                    CircleTime = Circle[2]
+
+                    Circle2 = CircleIndex[i + 1].split(",")
+                    CircleTime2 = Circle2[2]
+
+                # Steam Obtained, Push to SteamList
+                StreamList.append(CurrentStream)
+            i = i + 1
+
+        TrueStreamList = list(x for x in StreamList if len(x) > 3)
+
+        return TrueStreamList
 
 
 def StreamFollowDifficulty(mapID):
-  pass
+    pass
+
 
 # for x in os.listdir('test/'):
 #   if x.endswith('.osu'):
 #     DectectStream('test/'+x)
 
-DectectStream('test/boogie.osu')
+DectectStream("test/boogie.osu")
+
